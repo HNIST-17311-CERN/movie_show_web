b7804a3b71a10ca49ef14024f47427480d08288d
CERN <2440539709@qq.com>
2026-04-27 20:44:21 +0800

    find

--- src/main/java/org/example/Servlet/MapperController.java
+++ src/main/java/org/example/Servlet/MapperController.java
@@ -3,6 +3,7 @@ package org.example.Servlet;
 import org.example.Entity.Movie_details;
 import org.example.Mapper.MovieMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Mapper")
+@CrossOrigin(origins = "*")  // 允许所有来源访问
 public class MapperController
 {
 
